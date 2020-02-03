f4492270271e6e244ab12592690157aefe7be6a9
Kartheek Nagulapati <Kartheek.nagulapati@outlook.com>
2020-02-02 18:47:31 -0700

    numeric value

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-(function (){
+var myApp = angular.module('myApp',[]);
 
-    'use strict';
+'use strict';
+
+myApp.controller('MyFirstController', function($scope) {
+  $scope.name = "";
+  $scope.value = 0;
+
+  $scope.display = function(){
+      var total = calculateNumericForString($scope.name); //get the total value
+      $scope.value = total;
+  }
+
+  function calculateNumericForString (string){
+    var t = 0;
+    for(var i = 0; i < string.length; i++){
+        t += string.charCodeAt(i);
+    }
     
-    angular.module('myFirstApp', [])
-    .controller('MyFirstController', function(){
+    return t;
+  }
 
 
-
-    });
-
-
-
-})();
+});
