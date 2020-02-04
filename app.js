f26125c04bdd53fc574c8295e37aaae0da224165
Kartheek Nagulapati <Kartheek.nagulapati@outlook.com>
2020-02-03 19:12:26 -0700

    upper, and charcode value

--- app.js
+++ app.js
@@ -2,23 +2,29 @@ var myApp = angular.module('myApp',[]);
 
 'use strict';
 
-myApp.controller('MyFirstController', function($scope) {
+myApp.controller('MyFirstController', function($scope, $filter) {
   $scope.name = "";
   $scope.value = 0;
 
-  $scope.display = function(){
-      var total = calculateNumericForString($scope.name); //get the total value
-      $scope.value = total;
-  }
-
-  function calculateNumericForString (string){
-    var t = 0;
-    for(var i = 0; i < string.length; i++){
-        t += string.charCodeAt(i);
+    $scope.display = function(){
+        var total = calculateNumericForString($scope.name); //get the total value
+        $scope.value = total;
     }
-    
-    return t;
-  }
+
+    $scope.turnUpper = function(){
+        var upperFilter = $filter('uppercase');
+        $scope.upper = upperFilter($scope.name);
+    }
+
+
+    function calculateNumericForString (string){
+        var t = 0;
+        for(var i = 0; i < string.length; i++){
+            t += string.charCodeAt(i);
+        }
+
+        return t;
+    }
 
 
 });
