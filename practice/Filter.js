2cdaba2644a4fa5b616e419a0084138f2e81dae3
Kartheek Nagulapati <Kartheek.nagulapati@outlook.com>
2020-02-19 19:50:59 -0700

    filters

--- practice/Filter.js
+++ practice/Filter.js
@@ -2,24 +2,53 @@
 
     'use strict';
 
-    angular.module('myFilter', [])
-    .controller('FilterController', FilterController);
+    angular.module('myApp',[])
+    .controller('LunchCheckController', LunchCheckController);
 
-    FilterController.$inject = ['$scope', '$filter'];
-    
-    function FilterController($scope, $filter){
-        $scope.Name = "Kartheek"
+    //inject dependencies using $inject
+    LunchCheckController.$inject = ['$scope', '$filter'];
 
-        var message  = "Kartheek";
-        $scope.sayMessage = function (){
-            var output = $filter('uppercase')(message);
-            alert(1);
+    function LunchCheckController($scope, $filter){
+        
+        //ng-click on button
+        $scope.checkDishes = function(){
+
+            //get the value from text-box
+            var message = calculateNumberOfDishes($scope.dishes);
+            $scope.message = message;
         }
-    };
 
-    
+        function calculateNumberOfDishes(value){
+            if(value != undefined){
+                
+                var values = value.replace(/\s/g, "").split(',');
+                var finalvalues = removeFalsyElementsFromArray(values);
 
 
+                if(finalvalues.length <= 3 && finalvalues.length > 0){
+                    return "Enjoy!";
+                    $scope.colorClass = "Green";
+                } else if(finalvalues.length > 3){
+                    return "Too much!";
+                    $scope.colorClass = "Green";
+                } else{
+                    return "Please enter data first";
+                    $scope.colorClass = "Red";
+                }
+            } else{
+                return "Please enter data first";
+            }
+        }
 
+        function removeFalsyElementsFromArray(someArray) {
+            var newArray = [];
+            for(var index = 0; index < someArray.length; index++) {
+                if(someArray[index]) {
+                    newArray.push(someArray[index]);
+                }
+            }
+            return newArray;
+        }
+    }
 
-})
+})();
